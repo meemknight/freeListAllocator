bff2daeb84553c0ad97d41d7a515d86d191fd94f
unknown <vladalex420@gmail.com>
2020-10-19 16:15:24 +0300

    save

--- freeListAllocator/main.cpp
+++ freeListAllocator/main.cpp
@@ -4,49 +4,9 @@
 #include <array>
 #include <random>
 
-
 int main()
 {
 
-	std::vector<int> test;
-	test.push_back(1);
-	test.push_back(2);
-	test.push_back(3);
-	test.push_back(4);
-	test.push_back(5);
-	test.push_back(6);
-	test.push_back(7);
-	test.push_back(8);
-	test.push_back(9);
-	test.push_back(10);
-	test.push_back(11);
-	test.push_back(12);
-	test.push_back(13);
-	test.push_back(14);
-	test.push_back(15);
-	test.push_back(16);
-	test.push_back(17);
-	test.push_back(18);
-
-	std::vector<int> test2;
-	test2.push_back(1);
-	test2.push_back(2);
-	test2.push_back(3);
-	test2.push_back(4);
-	test2.push_back(5);
-	test2.push_back(6);
-	test2.push_back(7);
-	test2.push_back(8);
-	test2.push_back(9);
-	test2.push_back(10);
-	test2.push_back(11);
-	test2.push_back(12);
-	test2.push_back(13);
-	test2.push_back(14);
-	test2.push_back(15);
-	test2.push_back(16);
-	test2.push_back(17);
-	test2.push_back(18);
 
 	return 0;
 }
